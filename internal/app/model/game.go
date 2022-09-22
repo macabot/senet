3fed6a637b623be04b498839bef266268acd3c13
macabot <1422579+macabot@users.noreply.github.com>
2022-09-22 22:07:19 +0200

    Work on models

---
+++ internal/app/model/game.go
@@ -0,0 +1,18 @@
+package model
+
+type Status int
+
+const (
+	Created Status = iota
+	Ready
+	InProgress
+	Finished
+)
+
+type Game struct {
+	Board  Board
+	Sticks Sticks
+	You    int
+	Turn   int
+	Status Status
+}
---
+++ internal/app/model/sticks.go
@@ -0,0 +1,6 @@
+package model
+
+type Sticks struct {
+	Up        [4]bool
+	HasThrown bool
+}
