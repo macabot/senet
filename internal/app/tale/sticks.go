a955c3f0ad908672b01457c73b0cb016e045c32a
macabot <1422579+macabot@users.noreply.github.com>
2023-06-24 21:56:46 +0200

    Use steps control in sticks tale

--- internal/app/tale/sticks.go
+++ internal/app/tale/sticks.go
@@ -2,10 +2,10 @@ package tale
 
 import (
 	"github.com/macabot/fairytale"
-	"github.com/macabot/fairytale/control"
 	"github.com/macabot/hypp"
 	"github.com/macabot/senet/internal/app/component"
 	"github.com/macabot/senet/internal/app/state"
+	mycontrol "github.com/macabot/senet/internal/app/tale/control"
 )
 
 func Sticks() *fairytale.Tale[*state.State] {
@@ -21,24 +21,6 @@ func Sticks() *fairytale.Tale[*state.State] {
 			})
 		},
 	).WithControls(
-		control.NewButton(
-			"Throw",
-			func(s *state.State) *state.State {
-				s.Game.SetSticks(s.Game.Sticks.Throw())
-				return s
-			},
-		),
-		control.NewCheckbox(
-			"Has thrown",
-			func(s *state.State, hasThrown bool) *state.State {
-				sticks := s.Game.Sticks
-				sticks.HasThrown = hasThrown
-				s.Game.SetSticks(sticks)
-				return s
-			},
-			func(s *state.State) bool {
-				return s.Game.Sticks.HasThrown
-			},
-		),
+		mycontrol.Steps(),
 	)
 }
