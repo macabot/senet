079eb1b937435543573ec88c77e0b186e501bddc
macabot <1422579+macabot@users.noreply.github.com>
2023-11-10 22:27:07 +0100

    Generate and verify hash for commitment scheme

--- internal/app/state/commitmentscheme.go
+++ internal/app/state/commitmentscheme.go
@@ -4,17 +4,13 @@ package state
 
 import (
 	"crypto/rand"
+	"crypto/subtle"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"strings"
 
 	"golang.org/x/crypto/argon2"
-)
-
-type CommitmentStep int
-
-const (
-	NoCommitmentStep CommitmentStep = iota
-	GenerateFlipperSecretStep
-	SendFlipperSecretStep
-	GenerateCallerSecretStep
 )
 
 type CommitmentScheme struct {
@@ -31,7 +27,7 @@ func (c CommitmentScheme) Clone() CommitmentScheme {
 	return c
 }
 
-func generateSecret(size int) []byte {
+func generateRandomBytes(size int) []byte {
 	b := make([]byte, size)
 	_, err := rand.Read(b)
 	if err != nil {
@@ -40,35 +36,28 @@ func generateSecret(size int) []byte {
 	return b
 }
 
-func (c *CommitmentScheme) GenerateCallerSecret() {
-	c.CallerSecret = string(generateSecret(10))
+func generateRandomBytesBase64(size int) []byte {
+	return []byte(base64.RawStdEncoding.EncodeToString(generateRandomBytes(size)))
 }
 
-func (c *CommitmentScheme) GenerateFlipperSecret() {
-	c.FlipperSecret = string(generateSecret(10))
+const secretPlainSize = 10
+
+var secretBase64Size = base64.RawStdEncoding.EncodedLen(secretPlainSize)
+
+func GenerateSecret() string {
+	return string(generateRandomBytesBase64(secretPlainSize))
 }
 
-func (c *CommitmentScheme) GenerateCallerPredictions() {
-	c.CallerPredictions = [4]bool{
+func GenerateFlips() [4]bool {
+	return [4]bool{
 		defaultRNG.Intn(2) == 1,
 		defaultRNG.Intn(2) == 1,
 		defaultRNG.Intn(2) == 1,
 		defaultRNG.Intn(2) == 1,
 	}
-	c.HasCallerPredictions = true
 }
 
-func (c *CommitmentScheme) GenerateFlipperResults() {
-	c.FlipperResults = [4]bool{
-		defaultRNG.Intn(2) == 1,
-		defaultRNG.Intn(2) == 1,
-		defaultRNG.Intn(2) == 1,
-		defaultRNG.Intn(2) == 1,
-	}
-	c.HasFlipperResults = true
-}
-
-func (c *CommitmentScheme) GenerateCommitment() {
+func createPlainTextCommitment(callerSecret, flipperSecret string, callerPredictions [4]bool) []byte {
 	bToS := func(b bool) string {
 		if b {
 			return "1"
@@ -76,17 +65,103 @@ func (c *CommitmentScheme) GenerateCommitment() {
 		return "0"
 	}
 
-	password := []byte(
-		c.CallerSecret +
-			c.FlipperSecret +
-			bToS(c.CallerPredictions[0]) +
-			bToS(c.CallerPredictions[1]) +
-			bToS(c.CallerPredictions[2]) +
-			bToS(c.CallerPredictions[3]),
+	plainTextCommitment := []byte(
+		callerSecret +
+			flipperSecret +
+			bToS(callerPredictions[0]) +
+			bToS(callerPredictions[1]) +
+			bToS(callerPredictions[2]) +
+			bToS(callerPredictions[3]),
 	)
-	salt := generateSecret(32)
-	b := argon2.IDKey(password, salt, 1, 64*1024, 4, 32)
-	c.Commitment = string(b)
+	return plainTextCommitment
+}
+
+// GenerateCommitmentHash is based on https://www.alexedwards.net/blog/how-to-hash-and-verify-passwords-with-argon2-in-go
+func GenerateCommitmentHash(callerSecret, flipperSecret string, callerPredictions [4]bool) string {
+	if len(callerSecret) != secretBase64Size {
+		panic("cannot generate commitment hash with caller secret of invalid length")
+	}
+	if len(flipperSecret) != secretBase64Size {
+		panic("cannot generate commitment hash with flipper secret of invalid length")
+	}
+
+	password := createPlainTextCommitment(callerSecret, flipperSecret, callerPredictions)
+	const (
+		time    = 1
+		memory  = 64 * 1024
+		threads = 4
+		keyLen  = 32
+	)
+	salt := generateRandomBytes(keyLen)
+	hash := argon2.IDKey(password, salt, time, memory, threads, keyLen)
+
+	b64Salt := base64.RawStdEncoding.EncodeToString(salt)
+	b64Hash := base64.RawStdEncoding.EncodeToString(hash)
+	encodedHash := fmt.Sprintf("$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%s", argon2.Version, memory, time, threads, b64Salt, b64Hash)
+
+	return encodedHash
+}
+
+type decodedCommitment struct {
+	Hash    []byte
+	Salt    []byte
+	Time    uint32
+	Memory  uint32
+	Threads uint8
+}
+
+// IsExpectedCommitment is based on https://www.alexedwards.net/blog/how-to-hash-and-verify-passwords-with-argon2-in-go
+func IsExpectedCommitment(callerSecret, flipperSecret string, callerPredictions [4]bool, commitment string) bool {
+	args, err := decodeCommitment(commitment)
+	if err != nil {
+		fmt.Printf("decodeCommitment err: %s\n", err)
+		return false
+	}
+
+	password := createPlainTextCommitment(callerSecret, flipperSecret, callerPredictions)
+	otherHash := argon2.IDKey(password, args.Salt, args.Time, args.Memory, args.Threads, uint32(len(args.Hash)))
+
+	return subtle.ConstantTimeCompare(args.Hash, otherHash) == 1
+}
+
+var (
+	ErrInvalidHash         = errors.New("the encoded hash is not in the correct format")
+	ErrIncompatibleVersion = errors.New("incompatible version of argon2")
+)
+
+// decodeCommitment is based on https://www.alexedwards.net/blog/how-to-hash-and-verify-passwords-with-argon2-in-go
+func decodeCommitment(commitment string) (decodedCommitment, error) {
+	vals := strings.Split(commitment, "$")
+	if len(vals) != 6 {
+		return decodedCommitment{}, ErrInvalidHash
+	}
+
+	var version int
+	_, err := fmt.Sscanf(vals[2], "v=%d", &version)
+	if err != nil {
+		return decodedCommitment{}, err
+	}
+	if version != argon2.Version {
+		return decodedCommitment{}, ErrIncompatibleVersion
+	}
+
+	p := decodedCommitment{}
+	_, err = fmt.Sscanf(vals[3], "m=%d,t=%d,p=%d", &p.Memory, &p.Time, &p.Threads)
+	if err != nil {
+		return decodedCommitment{}, err
+	}
+
+	p.Salt, err = base64.RawStdEncoding.Strict().DecodeString(vals[4])
+	if err != nil {
+		return decodedCommitment{}, err
+	}
+
+	p.Hash, err = base64.RawStdEncoding.Strict().DecodeString(vals[5])
+	if err != nil {
+		return decodedCommitment{}, err
+	}
+
+	return p, nil
 }
 
 func (c CommitmentScheme) Throw() int {
