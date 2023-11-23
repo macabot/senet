32bd305fe915e316b810688e5d7441996a811379
macabot <1422579+macabot@users.noreply.github.com>
2023-11-23 21:08:59 +0100

    Rename variable

--- internal/app/dispatch/navigate.go
+++ internal/app/dispatch/navigate.go
@@ -65,12 +65,12 @@ func ToSignalingPageAction() hypp.Action[*state.State] {
 	}
 }
 
-func ToOnlinePlayerVsPlayerAction(isPlayer1 bool) hypp.Action[*state.State] {
+func ToOnlinePlayerVsPlayerAction(isPlayer0 bool) hypp.Action[*state.State] {
 	return func(s *state.State, payload hypp.Payload) hypp.Dispatchable {
 		newState := s.Clone()
 		newState.Page = state.GamePage
 		newState.Game = state.NewGame()
-		if isPlayer1 {
+		if isPlayer0 {
 			newState.Game.TurnMode = state.IsPlayer1
 			newState.Game.Players[0].Name = "You"
 			newState.Game.Players[1].Name = "Opponent"
