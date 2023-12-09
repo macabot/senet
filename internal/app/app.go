09ab7671147cdb3a6806fd19fabe23447a9ae794
macabot <1422579+macabot@users.noreply.github.com>
2023-12-09 15:02:07 +0100

    Persist state

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,18 +1,29 @@
 package app
 
 import (
+	"encoding/json"
+
 	"github.com/macabot/hypp"
 	"github.com/macabot/hypp/window"
 	"github.com/macabot/senet/internal/app/component"
 	"github.com/macabot/senet/internal/app/dispatch"
 	"github.com/macabot/senet/internal/app/state"
+	"github.com/macabot/senet/internal/pkg/localstorage"
 )
 
 func Run(element window.Element) {
+	s := &state.State{
+		Page: state.StartPage,
+	}
+	v, ok := localstorage.GetItem("state")
+	if ok {
+		if err := json.Unmarshal([]byte(v), s); err != nil {
+			panic(err)
+		}
+	}
+
 	hypp.App(hypp.AppProps[*state.State]{
-		Init: &state.State{
-			Page: state.StartPage,
-		},
+		Init: s,
 		View: component.Senet,
 		Node: element,
 		Subscriptions: func(s *state.State) []hypp.Subscription {
@@ -34,6 +45,25 @@ func Run(element window.Element) {
 					Subscriber: dispatch.OnDataChannelMessageSubscriber,
 					Disabled:   !initialized,
 				},
+			}
+		},
+		DispatchWrapper: func(dispatch hypp.Dispatch) hypp.Dispatch {
+			return func(dispatchable hypp.Dispatchable, payload hypp.Payload) {
+				dispatch(dispatchable, payload)
+				var s *state.State
+				switch v := dispatchable.(type) {
+				case hypp.StateAndEffects[*state.State]:
+					s = v.State
+				case *state.State: // State
+					s = v
+				default:
+					return
+				}
+				b, err := json.Marshal(s)
+				if err != nil {
+					panic(err)
+				}
+				localstorage.SetItem("state", string(b))
 			}
 		},
 	})
---
+++ internal/pkg/localstorage/localstorage.go
@@ -0,0 +1,19 @@
+package localstorage
+
+import (
+	"github.com/macabot/hypp/js"
+)
+
+// SetItem is based on https://developer.mozilla.org/en-US/docs/Web/API/Storage/setItem
+func SetItem(keyName, keyValue string) {
+	js.Global().Get("localStorage").Call("setItem", keyName, keyValue)
+}
+
+// GetItem is based on https://developer.mozilla.org/en-US/docs/Web/API/Storage/getItem
+func GetItem(keyName string) (string, bool) {
+	v := js.Global().Get("localStorage").Call("getItem", keyName)
+	if v.IsNull() {
+		return "", false
+	}
+	return v.String(), true
+}
