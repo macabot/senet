d5a0236676ab68f2ea82df07df70ae254cad974a
macabot <1422579+macabot@users.noreply.github.com>
2024-04-23 21:13:10 +0200

    Try to serve brotli or gzip file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 )
 
 func main() {
@@ -22,22 +23,57 @@ type Handler struct {
 	Dir http.Dir
 }
 
+var (
+	gzipPattern   = regexp.MustCompile(`\bgzip\b`)
+	brotliPattern = regexp.MustCompile(`\bbrotli\b`)
+)
+
 func (a *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL.Path)
-	http.FileServer(HTMLDir{a.Dir}).ServeHTTP(w, r)
+	tryBrotli := false
+	tryGzip := false
+	if len(r.Header["Accept-Encoding"]) > 0 {
+		tryBrotli = brotliPattern.MatchString(r.Header["Accept-Encoding"][0])
+		tryGzip = gzipPattern.MatchString(r.Header["Accept-Encoding"][0])
+	}
+	htmlDir := HTMLDir{
+		Dir:       a.Dir,
+		TryBrotli: tryBrotli,
+		TryGzip:   tryGzip,
+	}
+	http.FileServer(htmlDir).ServeHTTP(w, r)
 }
 
 type HTMLDir struct {
-	Dir http.Dir
+	Dir       http.Dir
+	TryBrotli bool
+	TryGzip   bool
 }
 
 func (d HTMLDir) Open(name string) (http.File, error) {
-	// Try name as supplied
-	f, err := d.Dir.Open(name)
-	if os.IsNotExist(err) {
-		// If not found, try with .html
-		if f, err := d.Dir.Open(name + ".html"); err == nil {
-			return f, nil
+	var tryNames []string
+	if d.TryBrotli {
+		tryNames = append(tryNames, name+".br")
+	}
+	if d.TryGzip {
+		tryNames = append(tryNames, name+".gz")
+	}
+	tryNames = append(tryNames, name)
+
+	if d.TryBrotli {
+		tryNames = append(tryNames, name+".html.br")
+	}
+	if d.TryGzip {
+		tryNames = append(tryNames, name+".html.gz")
+	}
+	tryNames = append(tryNames, name+".html")
+
+	var f http.File
+	var err error
+	for _, tryName := range tryNames {
+		f, err = d.Dir.Open(tryName)
+		if err == nil || !os.IsNotExist(err) {
+			break
 		}
 	}
 	return f, err
